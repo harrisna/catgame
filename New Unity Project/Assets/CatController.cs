4c27287fdcf9e1c838e624d5f387b3899debe1de
harrisna <harrisna@uwec.edu>
2018-03-02 14:44:12 -0600

    movement stuff

--- New Unity Project/Assets/CatController.cs
+++ New Unity Project/Assets/CatController.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class CatController : MonoBehaviour {
 	[SerializeField] private float moveSpeed = 10.0f;
 	[SerializeField] private float jumpHeight = 10.0f;
+	[SerializeField] private float maxSpeed = 10.0f;
 	[SerializeField] private float friction = 0.8f;
 
 	Rigidbody2D rb;
+	bool isJumping = false;
 
 	// Use this for initialization
 	void Start () {
@@ -18,12 +20,15 @@ public class CatController : MonoBehaviour {
 	void Update () {
 		float moveDir = Input.GetAxis("Horizontal");
 		Vector2 moveVec = new Vector2(moveSpeed * moveDir, 0.0f);
-		Vector2 frictionVec = moveVec * -friction;
 
-		if (Input.GetButton ("Jump")) {
+		if (rb.velocity.magnitude > maxSpeed || moveVec.x == 0.0f)
+			moveVec += new Vector2(rb.velocity.x * -friction, 0.0f);
+
+		if (Input.GetButton ("Jump") && !isJumping) {
+			isJumping = true;
 			moveVec += new Vector2(0.0f, jumpHeight);
 		}
 
-		rb.velocity += moveVec + frictionVec;
+		rb.velocity += moveVec;
 	}
 }
