9d4cf25da51d1127afc4f448437e49840d02fa7b
PARTY MAN X <4970541+PARTYMANX@users.noreply.github.com>
2018-03-12 21:16:32 -0500

    Death controller

---
+++ Assets/DeathController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathController : MonoBehaviour {
+
+	[SerializeField] private GameObject Dead;
+	[SerializeField] private GameObject Alive;
+
+	[SerializeField] private bool isAlive = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Alive.SetActive (isAlive);
+		Dead.SetActive (!isAlive);
+	}
+}
