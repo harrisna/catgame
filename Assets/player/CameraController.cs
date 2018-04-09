2a60273eb11933f62ddefef27bc842a2c72c717e
PARTY MAN X <4970541+PARTYMANX@users.noreply.github.com>
2018-04-09 14:48:37 -0500

    In-class

--- Assets/player/CameraController.cs
+++ Assets/player/CameraController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
-	private Vector3 offset;
+	[SerializeField] private Vector3 offset;
 	//public Rigidbody2D rb;
 	public Transform playerTransform;
 
@@ -17,9 +17,9 @@ public class CameraController : MonoBehaviour {
 	void Update () {
 		//Camera Movement
 		Vector3 temp = playerTransform.position + offset;
-		temp.z = Camera.main.transform.position.z;
+		temp.z = transform.position.z;
 		//Y Modifier
 		temp.y = temp.y + 3;
-		Camera.main.transform.position = temp;
+		transform.position = temp;
 	}
 }
