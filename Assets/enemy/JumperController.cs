b460ef0033c387c7c69a7a4e9133a0a2ae782d1b
Keenan D <dehnkeka@uwec.edu>
2018-04-15 17:54:44 -0500

    Fixed camera and made a jumping enemy
    111

---
+++ Assets/enemy/JumperController.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumperController : MonoBehaviour {
+
+	private float lastSpeed;
+   [SerializeField] private  float speed = 1.0f;
+    [SerializeField] private float origin;
+	[SerializeField] private float distance =3.0f;
+	[SerializeField] private float directionSpeed = 1.0f;
+	// [SerializeField] private Transform wallCheckRight;
+    //[SerializeField] private Transform wallCheckLeft;
+	[SerializeField] private float ledgeCheckerOffset = 0.53125f;
+	[SerializeField] private float ledgeCheckerRaySize = 2f;
+	[SerializeField] private float wallCheckerOffset = 0.51f;
+	[SerializeField] private float wallCheckerRaySize = 0.365f;
+	[SerializeField] LayerMask groundMask;
+	private SpriteRenderer sp;
+	private Rigidbody2D rb;
+	private bool jumping;
+	private bool grounded;
+	// Use this for initialization
+	void Start () {
+		origin = transform.position.x;
+		lastSpeed = speed;
+		sp = GetComponent<SpriteRenderer>();
+		rb = GetComponent<Rigidbody2D>();
+		jumping = false;
+		grounded = true;
+	}
+	
+	void OnCollisionEnter2D(Collision2D col){
+        if(col.gameObject.name == "AlivePlayer" || col.gameObject.name == "DeadPlayer")
+        {
+            Destroy(col.gameObject);
+        }
+		
+		if(col.gameObject.name == "Tilemap"){
+			grounded = true;
+		}
+		
+	}
+	
+	void FixedUpdate()
+    {
+	//Ledge Checking
+      RaycastHit2D ledgeRight = Physics2D.Raycast(new Vector2(transform.position.x+ledgeCheckerOffset,transform.position.y), Vector2.down, ledgeCheckerRaySize, groundMask);
+	  RaycastHit2D ledgeLeft = Physics2D.Raycast(new Vector2(transform.position.x-ledgeCheckerOffset,transform.position.y), Vector2.down, ledgeCheckerRaySize, groundMask);
+	  if (ledgeRight.collider == null) {
+		  
+            speed = -directionSpeed; //flip direction to left
+			
+      }else if (ledgeLeft.collider == null) {
+		  
+            speed = directionSpeed; //flip direction to  right
+			
+        }
+		//Wall checking
+		RaycastHit2D wallRight = Physics2D.Raycast(new Vector2(transform.position.x+wallCheckerOffset,transform.position.y), Vector2.right, wallCheckerRaySize, groundMask);
+		//
+		
+		//
+		RaycastHit2D wallLeft = Physics2D.Raycast(new Vector2(transform.position.x-wallCheckerOffset,transform.position.y), Vector2.left, wallCheckerRaySize, groundMask);
+		//
+		
+       // Debug.DrawRay(new Vector2(transform.position.x-wallCheckerOffset,transform.position.y), Vector2.left, Color.green);
+		//Debug.DrawRay(new Vector2(transform.position.x-ledgeCheckerOffset,transform.position.y), Vector2.down, Color.red);
+		
+		
+		if (wallRight.collider != null) {
+		  
+            speed = -directionSpeed; //flip direction to left
+			sp.flipX = false;
+      }else if (wallLeft.collider != null) {
+		  
+            speed = directionSpeed; //flip direction to  right
+			sp.flipX = true;
+        }
+		
+    }
+	
+	// Update is called once per frame
+	void Update () {
+			//Check if it is facing left or right and make sure the image is facing that direction
+			
+			if(speed == -directionSpeed){
+				sp.flipX=false;
+			}else{
+				sp.flipX = true;
+			}
+        
+			 
+             //transform.Translate(speed*Time.deltaTime,0,0);
+			 if(grounded){
+		rb.velocity = new Vector2 (0.0f, -rb.velocity.y + 500.0f * Time.fixedDeltaTime);
+		jumping = true;
+		grounded = false;
+			 }
+	}
+}
--- Assets/player/CameraController.cs
+++ Assets/player/CameraController.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//Camera size should be 13
 public class CameraController : MonoBehaviour {
-	[SerializeField] private Vector3 offset;
+	[SerializeField] private float yModifyer = 7;
+	[SerializeField] private float aliveXStaticPosition = -9;
+	[SerializeField] private float deadXStaticPosition = 16;
 	//public Rigidbody2D rb;
 	public Transform playerTransform;
 
 	// Use this for initialization
 	void Start () {
 		//offset
-		offset = playerTransform.position - transform.position;
+		
 	}
 	
 	// Update is called once per frame
@@ -19,7 +21,14 @@ public class CameraController : MonoBehaviour {
 		Vector3 temp = playerTransform.position;
 		temp.z = transform.position.z;
 		//Y Modifier
-		temp.y = temp.y + 3;
+		temp.y = temp.y + yModifyer;
+		//Static x camera value
+		if(playerTransform.name == "AlivePlayer"){
+		temp.x = aliveXStaticPosition;
+		}else{
+			temp.x = deadXStaticPosition;
+		}
+		//
 		transform.position = temp;
 	}
 }
