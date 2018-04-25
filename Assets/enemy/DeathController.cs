8021b2dc540a93fb38c875911cbc349d53357aa0
Keenan D <dehnkeka@uwec.edu>
2018-04-24 20:24:39 -0500

    Enemies now have dead bodies and come alive when they die

--- Assets/enemy/DeathController.cs
+++ Assets/enemy/DeathController.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class DeathController : MonoBehaviour {
 
 	[SerializeField] private GameObject enemy;
-
+	[SerializeField] private GameObject aliveBodyObject;
+	[SerializeField] private GameObject deadBodyObject;
 	[SerializeField] private Transform alivePos;
 	[SerializeField] private Transform deadPos;
 
 	private GameObject alive;
 	private GameObject dead;
+	private GameObject aliveBody;
+	private GameObject deadBody;
 
 	[SerializeField] private LifeController aliveLC;
     [SerializeField] private LifeController deadLC;
@@ -21,7 +24,8 @@ public class DeathController : MonoBehaviour {
 	void Start () {
 		alive = Instantiate (enemy, alivePos.position, alivePos.rotation);
 		dead = Instantiate (enemy, deadPos.position, deadPos.rotation);
-
+		aliveBody = Instantiate (aliveBodyObject, alivePos.position, alivePos.rotation);
+		deadBody = Instantiate (deadBodyObject, deadPos.position, deadPos.rotation);
 		aliveLC = alive.GetComponent<LifeController>();
         deadLC = dead.GetComponent<LifeController>();
 
@@ -33,10 +37,17 @@ public class DeathController : MonoBehaviour {
 	void Update () {
 		alive.SetActive (isAlive);
 		dead.SetActive (!isAlive);
+		aliveBody.SetActive (!isAlive);
+		deadBody.SetActive (isAlive);
+		
+		if(isAlive){
+			
+		}
 	}
 
     public void Kill() {
         isAlive = !isAlive;
+		
     }
 
 	void OnDrawGizmos() {
