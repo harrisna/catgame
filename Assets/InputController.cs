41d37afa228750db51e9c87d5f5e2f7418e3586b
harrisna <harrisna@uwec.edu>
2018-05-10 15:40:52 -0500

    input changes to accomodate controller

--- Assets/InputController.cs
+++ Assets/InputController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO.Ports;
 
 public enum InputButton { Left = 0, Right = 1, Up = 2, Down = 3, Jump = 4, Attack = 5 };
 
@@ -11,16 +12,34 @@ public class InputController : MonoBehaviour {
 		{ KeyCode.LeftArrow, KeyCode.RightArrow, KeyCode.UpArrow, KeyCode.DownArrow, KeyCode.RightShift, KeyCode.RightAlt }
 	};
 
-	public static bool GetButton(int player, InputButton b) {
-		return Input.GetKey(keymap[player - 1, (int)b]);
+    //private static readonly int[,] buttonmap = { { }, { } };
+
+    private static bool[,] buttonState = {{ false, false, false, false, false, false }, { false, false, false, false, false, false } };
+
+    private SerialPort serial;
+
+    void Start() {
+        serial = new SerialPort("/dev/tty.usbmodem1411", 9600);
+        serial.Open();
+    }
+
+
+    void Update() {
+        while (serial.BytesToRead > 0) {
+            buttonState[serial.ReadByte(), serial.ReadByte()] = (serial.ReadByte() == 1);    
+        }
+    }
+
+    public static bool GetButton(int player, InputButton b) {
+		return Input.GetKey(keymap[player - 1, (int)b]) || buttonState[player - 1, (int)b];
 	}
 
 	public static float GetAxis(int player) {
 		float result = 0.0f;
 
-		if (Input.GetKey(keymap[player - 1, (int)InputButton.Left]))
+		if (Input.GetKey(keymap[player - 1, (int)InputButton.Left]) || buttonState[player - 1, (int)InputButton.Left])
 			result += -1.0f;
-		if (Input.GetKey(keymap[player - 1, (int)InputButton.Right]))
+		if (Input.GetKey(keymap[player - 1, (int)InputButton.Right]) || buttonState[player - 1, (int)InputButton.Right])
 			result += 1.0f;
 
 		return result;
